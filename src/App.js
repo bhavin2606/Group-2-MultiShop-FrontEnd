43835d38a00d55b7e3bf62047d273ec3da98f593
Smitpatel0409 <psmit9077@gmail.com>
2024-06-24 15:52:34 +0530

    Dependency has been installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/common/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './assets/css/style.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
 
